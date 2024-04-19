cc1836806483dd7e92ea9200e5d7e33b01cc6fd6
Cédric <work@cedricverlinden.com>
2024-04-19 23:11:37 +0200

    Update App.java to use login.fxml instead of scene.fxml

--- app/src/main/java/com/cedricverlinden/banking/App.java
+++ app/src/main/java/com/cedricverlinden/banking/App.java
@@ -4,19 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/scene.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("css/buttons.css").toExternalForm());
-
-        stage.setTitle("Banking");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Banking");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("icons/icon.png")));
         stage.show();
     }
 
