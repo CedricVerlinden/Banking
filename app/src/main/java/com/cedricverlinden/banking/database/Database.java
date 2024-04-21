ae95d91dda9a52ec937b8a498aa599c34e6399b0
Cédric <work@cedricverlinden.com>
2024-04-22 00:19:28 +0200

    Refactor database connection code to use Database class

--- app/src/main/java/com/cedricverlinden/banking/database/Database.java
+++ app/src/main/java/com/cedricverlinden/banking/database/Database.java
@@ -1,4 +1,4 @@
-package com.cedricverlinden.banking;
+package com.cedricverlinden.banking.database;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -6,30 +6,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class MySQLConnect {
+public class Database {
     public static Connection connect() {
         try {
             Properties properties = new Properties();
-            properties.load(MySQLConnect.class.getClassLoader().getResourceAsStream("database.properties"));
+            properties.load(Connection.class.getClassLoader().getResourceAsStream("database.properties"));
 
             String driver = properties.getProperty("jdbc.driver");
             String url = properties.getProperty("jdbc.url");
             String username = properties.getProperty("jdbc.username");
             String password = properties.getProperty("jdbc.password");
 
-            System.out.println(driver + " " + url + " " + username + " " + password);
-
             Class.forName(driver);
             return DriverManager.getConnection(url, username, password);
 
         } catch (IOException e) {
-            e.printStackTrace();
             System.out.println("Failed to load properties file");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
             System.out.println("Failed to load driver");
         } catch (SQLException e) {
-            e.printStackTrace();
             System.out.println("Failed to connect to database");
         }
 
