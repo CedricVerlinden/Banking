0d4f8d7faabc61c2a71b10ecbb30511b415640ff
Cédric <work@cedricverlinden.com>
2024-04-26 21:54:12 +0200

    Add support for opening models package to JavaFX base module

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.cedricverlinden.banking {
     requires transitive javafx.graphics;
 
     opens com.cedricverlinden.banking to javafx.fxml;
+    opens com.cedricverlinden.banking.models to javafx.base;
     opens com.cedricverlinden.banking.controllers to javafx.fxml;
 
     exports com.cedricverlinden.banking;
